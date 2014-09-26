0a230b49018ed55753700ddaf0bd8b26c9fea179
zacck <osiemobiz@gmail.com>
2014-09-26 22:04:19 +0300

    had to fix the mainactivity so that we ccould implement log out

--- src/com/semasoft/niwapi/MainImageActivity.java
+++ src/com/semasoft/niwapi/MainImageActivity.java
@@ -13,8 +13,6 @@ public class MainImageActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_image);
-		
-	
 
 	}
 
