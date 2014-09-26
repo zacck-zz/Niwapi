ba4fe59b77b8db7d50ee6e75f062d1f638022ce2
zacck <osiemobiz@gmail.com>
2014-09-26 13:58:32 +0300

    added checking os server response for user reg

--- src/com/semasoft/niwapi/RegisterActivity.java
+++ src/com/semasoft/niwapi/RegisterActivity.java
@@ -13,6 +13,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -24,6 +25,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity implements OnClickListener {
 
@@ -85,6 +87,33 @@ public class RegisterActivity extends Activity implements OnClickListener {
 
 		switch (v.getId()) {
 		case R.id.btnRegister:
+			String un,
+			up,
+			ucp;
+			un = ETName.getText().toString();
+			up = ETPhone.getText().toString();
+			ucp = ETConfP.getText().toString();
+
+			if (un.isEmpty() || up.isEmpty() || ucp.isEmpty()) {
+				Toast.makeText(RegisterActivity.this,
+						"Please fill in all the fields to proceed",
+						Toast.LENGTH_LONG).show();
+
+			} else {
+				if (up.equalsIgnoreCase(ucp)) 
+				{
+					String[] ps = new String[]{up, un};
+					registerUser  ru = new registerUser();
+					ru.execute(ps);
+						
+
+				} else {
+					Toast.makeText(
+							RegisterActivity.this,
+							"Please confirm that the phone number is same as the confirmation",
+							Toast.LENGTH_LONG).show();
+				}
+			}
 
 			break;
 
@@ -93,32 +122,41 @@ public class RegisterActivity extends Activity implements OnClickListener {
 	}
 
 	class registerUser extends AsyncTask<String[], Void, Void> {
+		
+		String ServerResp = "";
 
 		@Override
 		protected Void doInBackground(String[]... params) {
 			try {
-				// call http methods
+				String[] data = params[0];
 				// Create a new HttpClient and Post Header
 				HttpClient httpclient = new DefaultHttpClient();
 				HttpPost httppost = new HttpPost(
-						"http://www.yoursite.com/script.php");
+						"http://www.appbase.co.ke/niwapi_rest/register_user.php");
 
 				// Add your data
 				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
 						2);
-				nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-				nameValuePairs.add(new BasicNameValuePair("stringdata",
-						"AndDev is Cool!"));
+				nameValuePairs.add(new BasicNameValuePair("user_name", data[0]));
+				nameValuePairs
+						.add(new BasicNameValuePair("user_phone", data[1]));
 				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
 				// Execute HTTP Post Request
 				HttpResponse response = httpclient.execute(httppost);
+				ServerResp = EntityUtils.toString(response.getEntity());
 
 			} catch (Exception e) {
 				Log.d(TAG, e.toString());
 			}
 
 			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			Log.d(TAG, ServerResp);
 		}
 
 	}
